d0ac5aee9149cf0b93e6b7a3fb666b2dfbe454d3
Nahom Melese <nahomjr17@gmail.com>
2024-10-17 20:37:58 -0700

    updated

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import { AddProducts } from "./pages/add_products/add_products"
+
+
+function App() {
+  
+
+  return (
+    <>
+   
+      <AddProducts></AddProducts>
+    </>
+  )
+}
+
+export default App
---
+++ admin/src/components/navbar/navbar.jsx
@@ -0,0 +1,9 @@
+import './navbar.css';
+
+export const NavBar = ()=>{
+    return <>
+        <div className="navbar">
+        kj
+        </div>
+    </>
+}
