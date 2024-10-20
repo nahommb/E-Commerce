46f2d4fa6e50001bddcc8677bc3b3a934fa48685
Nahom Melese <nahomjr17@gmail.com>
2024-10-19 20:07:05 -0700

    updated

--- admin/src/components/sidebar/sidebar.jsx
+++ admin/src/components/sidebar/sidebar.jsx
@@ -1,28 +1,34 @@
 import './sidebar.css'
 import {Button} from '@mui/material'
-import { ShoppingCart, Folder,} from '@mui/icons-material';
+import { ShoppingCart, Folder, ShowChart,Logout} from '@mui/icons-material';
 
 
 export const SideBar = ()=>{
     return <>
         <div className='side-bar'>
-            <div className="add-product">
+            <div className="button-cont">
                <Button style={{color:'white'}}>
                  <ShoppingCart style={{paddingRight:'8px'}}/>
                   Add products
                </Button>
             </div>
-            <div className="list-of-product">
+            <div className="button-cont">
             <Button style={{color:'white'}}>
                  <Folder style={{paddingRight:'8px'}}/>
                   Product List
                </Button>
             </div>
-            <div className="list-of-product">
+            <div className="button-cont">
             <Button style={{color:'white'}}>
-                 
+                 <ShowChart style={{paddingRight:'8px'}}/>
                   Statestics
-               </Button>
+               </Button> 
+            </div>
+            <div className="button-cont">
+            <Button style={{color:'white'}}>
+                 <Logout style={{paddingRight:'8px'}}/>
+                  Logout
+               </Button> 
             </div>
         </div>
     </>
