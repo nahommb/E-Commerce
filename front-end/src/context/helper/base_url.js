5bc0f8db347cffd1b68790a1a785267fb272be32
Nahom Melese <nahomjr17@gmail.com>
2024-12-15 17:21:20 -0800

    updated

--- front-end/src/context/helper/base_url.js
+++ front-end/src/context/helper/base_url.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:3000/api/";
-export const imageUrl = 'http://localhost:3000/';
+export const baseUrl = "https://sports-wear.onrender.com/api/";
+export const imageUrl = 'https://sports-wear.onrender.com/';
