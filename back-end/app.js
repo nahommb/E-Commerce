306a93853021543c1bc77bbd60d1c258b6e76177
Nahom Melese <nahomjr17@gmail.com>
2024-12-20 17:11:55 -0800

    updetd

--- back-end/app.js
+++ back-end/app.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'http://niyasportswear.netlify.app',
+      'https://niyasportswear.netlify.app',
   ], 
   methods: 'GET, POST, PUT, DELETE',
   credentials: true, 
