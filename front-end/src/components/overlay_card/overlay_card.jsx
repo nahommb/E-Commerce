3bd519d495fc4fc3069b4e57410c7cff10e6be3c
Nahom Melese <nahomjr17@gmail.com>
2024-12-24 20:54:40 -0800

    updated

--- front-end/src/components/overlay_card/overlay_card.jsx
+++ front-end/src/components/overlay_card/overlay_card.jsx
@@ -17,7 +17,37 @@ if(props.title === 'Order response'){
   setIsLoading(false)
 }
 
+const isSmallScreen = window.matchMedia("(max-width: 480px)").matches;
 
+const styles = {
+  overlay: {
+    position: 'fixed',
+    top: 100,
+    left: isSmallScreen ? '10%' : '40%',
+    right: isSmallScreen ? '10%' : '50%',
+    height:'300px',
+    width: '300px',
+    bottom: 0,
+    borderRadius:0,
+    border:'none',
+    boxShadow: 'none',
+    backgroundColor: 'transparent',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 9999,
+  },
+  popup: {
+    backgroundColor: 'whitesmoke',
+    padding: '20px',
+    // height: '130px',
+    borderRadius: '8px',
+    textAlign: 'center',
+    width: '300px',
+   
+  }
+  
+};
  const orderHandler = ()=>{
   props.onClick({
     name:name,
@@ -48,7 +78,7 @@ if(props.title === 'Order response'){
       <h3 style={{color:'black'}}>{props.title}</h3>
       <p style={{color:'black'}}>{props.message}</p><br/>
       <Button onClick={props.onClick}>{props.button_text}</Button>
-      <Button style={{color:'red'}} onClick={props.onCancel} >Cancel</Button>
+      {props.onCancel && <Button style={{color:'red'}} onClick={props.onCancel} >Cancel</Button>}
       </>
       
       }</div>}
@@ -58,31 +88,3 @@ if(props.title === 'Order response'){
   </section>
 }
 
-const styles = {
-    overlay: {
-      position: 'fixed',
-      top: 100,
-      left: '20%',
-      right: '10%',
-      height:'300px',
-      width: '300px',
-      bottom: 0,
-      borderRadius:0,
-      border:'none',
-      boxShadow: 'none',
-      backgroundColor: 'transparent',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      zIndex: 9999,
-    },
-    popup: {
-      backgroundColor: 'whitesmoke',
-      padding: '20px',
-      height: '150px',
-      borderRadius: '8px',
-      textAlign: 'center',
-      width: '300px',
-     
-    }
-  };
